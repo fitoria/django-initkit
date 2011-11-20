096e69992bee5ec45eb7664e57420d0fc9ee810d
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2011-11-20 18:14:41 -0300

    second commit added setup

--- initkit/django_initkit.py
+++ initkit/django_initkit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import os, sys
 
-INSTALLED_APPS = ('init_app',)
+INSTALLED_APPS = ('initkit.init_app',)
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_init")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_initkit")
 
     from django.core.management import execute_from_command_line
 
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys
+
+install_requires = ['django>=1.3']
+
+
+setup(
+    name = "django-initkit",
+    version = '0.0.1',
+    url = 'http://fitoria.net/',
+    author = 'Adolfo Fitoria',
+    author_email = 'adolfo.fitoria@gmail.com',
+    description = 'A personalized django startproject command',
+    packages = find_packages(),
+    scripts = ['initkit/django-initkit.py'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+   ],
+)
